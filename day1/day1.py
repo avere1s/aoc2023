275dfaf8cd83d964258912ba7c5166da46790732
Alexandra Veremey <alexandra@veremey.net>
2023-12-01 20:27:00 -0800

    Renamed character to digit

--- day1/day1.py
+++ day1/day1.py
@@ -11,36 +11,36 @@ for line in sys.stdin:
     print("")
 
     first_digit = -1
-    first_character_position = 0
-    for a_character in line.rstrip():
-        first_character_position += 1
-        print(f"    {a_character}: {ord(a_character)}")
+    first_digit_position = 0
+    for a_digit in line.rstrip():
+        first_digit_position += 1
+        print(f"    {a_digit}: {ord(a_digit)}")
 
-        if ord(a_character) > 47 and ord(a_character) < 58:
-            print(f"Found digit: {a_character}, position: {first_character_position}")
-            first_digit = int(a_character)
+        if ord(a_digit) > 47 and ord(a_digit) < 58:
+            print(f"Found digit: {a_digit}, position: {first_digit_position}")
+            first_digit = int(a_digit)
             break
 
     if first_digit == -1:
         print("First digit not found")
         exit(1)
 
-    if first_character_position == len(line.rstrip()):
-        print("First digit found at the last line character")
+    if first_digit_position == len(line.rstrip()):
+        print("First digit found at the last line digit")
 
         sum += first_digit * 10 + first_digit
         print(f"Sum: {sum}. Added {first_digit * 10 + first_digit}")
         continue
 
     second_digit = -1
-    print("Characters in reverse:")
-    second_character_position = len(line.rstrip())
-    for index in range(len(line.rstrip()) - 1, first_character_position - 1, -1):
-        second_character_position -= 1
+    print("digits in reverse:")
+    second_digit_position = len(line.rstrip())
+    for index in range(len(line.rstrip()) - 1, first_digit_position - 1, -1):
+        second_digit_position -= 1
         print(f"    {line[index]}: {ord(line[index])}")
 
         if ord(line[index]) > 47 and ord(line[index]) < 58:
-            print(f"Found digit: {line[index]}, position: {second_character_position}")
+            print(f"Found digit: {line[index]}, position: {second_digit_position}")
             second_digit = int(line[index])
             break
 
